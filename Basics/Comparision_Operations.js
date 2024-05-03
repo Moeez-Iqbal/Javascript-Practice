1dd9011371a2ed2bdfdd4227551c92dec5e20010
Moeez Iqbal <110253437+Moeez-Iqbal@users.noreply.github.com>
2024-05-03 12:40:04 +0000

    Comparision Operations.js

---
+++ Basics/Comparision_Operations.js
@@ -0,0 +1,22 @@
+// Comparision operator
+
+
+/*console.log(5<6);
+/*console.log(5<=6);
+console.log(5>=6);        Clean code is Priority
+console.log(5==6);
+console.log(5===6);
+console.log(5!=6);*/
+
+//console.log("2">1);
+//console.log("02">1);
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)// It will give True beacuse coversion but The best practice is not to compare different data types
+
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined>=0)
+
+console.log("2"===2); // It will strictly compare means compare dataTypes
