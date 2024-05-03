379a8692cb749dd1522e82158d39bda03a772d46
Moeez Iqbal <110253437+Moeez-Iqbal@users.noreply.github.com>
2024-05-03 11:57:14 +0000

    DataTypes and ECMA

---
+++ Basics/Datatypes_ECMA.js
@@ -0,0 +1,22 @@
+"Use Strict"; // treat all js code as newer version of javascript
+
+alert(3 + 3) // It will not run in terminal it has another way to run in terminal will discuss in other videos alert will run in console of browser
+
+/* code Readability should be high
+console.log(3
+ +3) Its a not goodpractice but It will execute
+should write as
+console.log(3+3)
+console.log("Moeez") */
+
+
+// number => range 2 to power 53
+// Bigint for big numbers
+// string=> ""
+// boolean => " True or False"
+// null => standalone value
+// undefined =>
+// symbol => uniqueness
+//object 
+
+// null is an object, undefined is undefined using typeof
