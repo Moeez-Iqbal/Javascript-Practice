a0670e4bf88f6c112c0c52fa44b54ff39b5630ed
Moeez Iqbal <110253437+Moeez-Iqbal@users.noreply.github.com>
2024-05-03 12:29:04 +0000

    Conversion Operations

---
+++ Basics/Conversion_Operations.js
@@ -0,0 +1,34 @@
+let value = 7;
+let negValue = -value;
+
+console.log(negValue);
+
+
+// Arithmetic Operators
+console.table([(3 + 3), (5 - 2), (6 * 4), (2 ** 2), /* 2 to power two*/(2 ** 3), /* 2 To power three*/(10 / 5), (10 % 5)])
+
+
+
+let str1 = "Moeez"
+let str2 = " Iqbal"
+let str3 = str1 + str2;
+
+console.log(str3);
+
+// Complex Situation
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1"+ "2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(true) //1
+console.log(+true) //1
+// not possible console.log(true+)
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2 // Not prefered
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
